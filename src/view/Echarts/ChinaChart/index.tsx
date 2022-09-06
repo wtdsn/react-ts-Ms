7d0d954ba14b12d1cbc6038db379dc5ab4a6dbde
‘huangwantao’ <2747964040@qqcom.com>
2022-09-07 00:40:53 +0800

    map suc

--- src/view/Echarts/ChinaChart/index.tsx
+++ src/view/Echarts/ChinaChart/index.tsx
@@ -3,11 +3,14 @@ import { FC, useState, useEffect, useMemo } from 'react'
 import { throttle } from '@/utils/index';
 import './index.less'
 
-import JSONDATA from '@/assets/JSON/6.json'
+import geoJson from '@/assets/JSON/china.json'
+import areaData from './area'
 
-/* echarts */
+/* echarts 按需导入 */
 import * as echarts from 'echarts/core';
 
+/* 导入相关的 component 比如 title , toolbox 等 */
+/* 同时导入相关的 option */
 import {
   TitleComponent,
   TitleComponentOption,
@@ -30,41 +33,78 @@ import {
   CanvasRenderer
 } from 'echarts/renderers';
 
+
+echarts.use([
+  TitleComponent,
+  ToolboxComponent,
+  TooltipComponent,
+  VisualMapComponent,
+  GeoComponent,
+  MapChart,
+  CanvasRenderer
+]);
+
 type EChartsOption = echarts.ComposeOption<
   TitleComponentOption | ToolboxComponentOption | TooltipComponentOption | VisualMapComponentOption | GeoComponentOption | MapSeriesOption
 >
 
-echarts.use(
-  [TitleComponent, ToolboxComponent, TooltipComponent, VisualMapComponent, GeoComponent, MapChart, CanvasRenderer]
-);
-
-echarts.registerMap('chinaChart', JSONDATA as any)
+echarts.registerMap('chinaMap', geoJson as any)
 
 const ChianChart: FC = () => {
   const [Charts, setC] = useState<echarts.ECharts>()
   const [Con, setCon] = useState<HTMLDivElement>()
 
   const option: EChartsOption = useMemo(() => {
+    let data = areaData
     return {
       title: {
-        text: "chinaChart",
-        series: [{
-          type: 'map',
-          mapType: 'chinaChart', // 需要与注册时设置的名称相同
-        }]
-      }
+        text: "China Map",
+        link: 'https://echarts.apache.org/examples/zh/editor.html?c=map-HK',
+        subtext: 'use echarts',
+        sublink: 'https://echarts.apache.org/zh/index.html'
+      },
+      series: [{
+        type: 'map',
+        map: 'chinaMap',
+        roam: true,
+        scaleLimit: {
+          min: 1,
+          max: 10
+        },
+        label: {
+          show: true
+        },
+        data
+      }],
+      visualMap: {
+        min: 0,
+        max: 170, calculable: true,
+        text: ['High', 'Low'],
+        inRange: {
+          color: ['lightskyblue', 'yellow', 'orangered']
+        }
+      },
+      tooltip: {
+        trigger: 'item',
+        formatter: '{b}<br/>{c} 万平方公里'
+      },
+      toolbox: {
+        show: true,
+        itemSize: 20,
+        orient: 'vertical',
+        right: '2%',
+        top: 'center',
+        feature: {
+          dataView: { readOnly: false },
+          restore: {},
+          saveAsImage: {},
+          // magicType: {
+          //   type: ['line', 'bar', 'stack']
+          // }
+        }
+      },
     }
   }, [])
-
-  /*  const MapData: any = useMemo(() => {
-     let features: any = [], res = { "type": "FeatureCollection", features }
-     JSONDATA.features.forEach((v, i) => {
-       features.push({
- 
-       })
-     })
-     return res
-   }, []) */
 
   useEffect(() => {
     if (!Con) {
@@ -107,7 +147,7 @@ const ChianChart: FC = () => {
     <div className='china_echart_con'>
       <div className="c_eachrt" style={{
         width: '100%',
-        height: '80vh'
+        height: '100%'
       }} ></div>
     </div >)
 }
